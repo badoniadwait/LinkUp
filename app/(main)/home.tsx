df24b8d078e36a3abe7b803c7e916daa46057230
Adwait Badoni <badoniadwait@gmail.com>
2026-08-07 17:44:26 +0530

    show posts

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,18 +1,62 @@
 import Icon from '@/assets/icons'
 import { Avatar } from '@/components/Avatar'
+import Loading from '@/components/Loading'
+import PostCard from '@/components/PostCard'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import { theme } from '@/constants/theme'
 import { useAuth } from '@/contexts/AuthContext'
 import { hp, wp } from '@/helpers/common'
+import { supabase } from '@/lib/supabase'
+import { fetchPosts } from '@/service/postService'
+import { getUserdata } from '@/service/userService'
 import { useRouter } from 'expo-router'
-import React from 'react'
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 
+let limit = 0;
 const Home = () => {
 
   const router = useRouter();
 
   const { user, setAuth } = useAuth();
+
+  const [posts, setPosts] = useState([]);
+
+
+  const getPosts = async () => {
+    limit = limit + 10;
+
+    console.log('fetching posts: ', limit);
+
+    const res = await fetchPosts(limit);
+
+    if (res.success) {
+      setPosts(res.data);
+    }
+  }
+
+  async function handlePostEvent(payload) {
+    if (payload.eventType == 'INSERT' && payload?.new?.id) {
+      let newPost = { ...payload.new };
+      let res = await getUserdata(newPost.usserId);
+      newPost.user = res.success ? res.data : {};
+      setPosts((prev) => [newPost, ...prev]);
+    }
+  }
+
+  useEffect(() => {
+
+    let postChannel = supabase
+      .channel('posts')
+      .on('postgres_changes', { event: '*', schema: 'public', table: 'posts' }, handlePostEvent)
+      .subscribe();
+
+    getPosts();
+
+    return () => {
+      supabase.removeChannel(postChannel);
+    }
+  }, []);
 
   // async function onLogout() {
   //   setAuth(null);
@@ -30,8 +74,31 @@ const Home = () => {
             <Pressable onPress={() => router.push('/(main)/notifications')}><Icon name='heart' size={hp(3.2)} strokeWidth={2} color={theme.colors.text} /></Pressable>
             <Pressable onPress={() => router.push('/(main)/newPost')}><Icon name='plus' size={hp(3.2)} strokeWidth={2} color={theme.colors.text} /></Pressable>
             <Pressable onPress={() => router.push('/(main)/profile')}><Avatar uri={user?.image} size={hp(4.3)} rounded={theme.radius.sm} style={{ borderWidth: 2 }} /></Pressable>
+
           </View>
         </View>
+        <FlatList
+          data={posts}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.listStyle}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={
+            ({ item }) => <PostCard
+              item={item}
+              currentUser={user}
+              router={router}
+            />
+          }
+          ListFooterComponent={
+            (
+              <View style={{
+                marginVertical: posts.length == 0 ? 200 : 30
+              }}>
+                <Loading />
+              </View>
+            )
+          }
+        />
       </View>
     </ScreenWrapper>
   )
@@ -59,4 +126,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 18,
   },
+  listStyle: {
+    paddingHorizontal: wp(4),
+    paddingTop: 20,
+  }
 })
--- service/postService.ts
+++ service/postService.ts
@@ -31,3 +31,25 @@ export async function createUpdatePost(post: {
         return { success: false, msg: 'could not create post' };
     }
 }
+
+export async function fetchPosts(limit = 10) {
+    try {
+    const {data, error} = await supabase.from('posts')
+    .select('*, user: users (id, name, image)')
+    .order('created_at', {ascending: false}).limit(limit);
+
+    if(error) {
+        console.log('fetch post error ', error);
+        return { success: false, msg: 'could not fetch post' };
+    }
+    else {
+        return { success: true, data };
+
+    }
+
+    } catch (error) {
+        console.log('fetch post error ', error);
+        return { success: false, msg: 'could not fetch post' };
+    }
+
+}
